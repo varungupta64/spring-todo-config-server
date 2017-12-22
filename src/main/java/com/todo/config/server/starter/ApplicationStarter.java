3612d0b52b1b1aeb5b445685647b92c2cb526e49
varungupta64 <varungupta0064@gmail.com>
2017-12-22 20:10:47 +0530

    Initial Commit

---
+++ src/main/java/com/todo/config/server/starter/ApplicationStarter.java
@@ -0,0 +1,13 @@
+package com.todo.config.server.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ApplicationStarter {
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationStarter.class, args);
+	}
+}
